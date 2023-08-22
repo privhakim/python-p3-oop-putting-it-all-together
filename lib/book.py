dc4bf2aa39a63224e29751fd4683ef97b80d509c
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-08-22 15:47:46 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!") 
+
+if __name__ == "__main__":
+    book = Book("And Then There Were None", 272)
+    print(book.title)
+    print(book.page_count)
+    book.turn_page()
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+if __name__ == "__main__":
+    stan_smith = Shoe("Adidas", 9)
+    print(stan_smith.brand)
+    print(stan_smith.size)
+    stan_smith.cobble()
+    print(stan_smith.condition)
